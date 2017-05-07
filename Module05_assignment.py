539a383af6e8362fabeafcc8bdd27065ec6234b1
Noel Runyan <runyanorama@users.noreply.github.com>
2017-05-07 18:30:03 -0500

    Update Module05_assignment.py

--- Module05_assignment.py
+++ Module05_assignment.py
@@ -1,19 +1,14 @@
 # 1) Create a text file called todo.txt and using some data (tasks, priorities).
+objFile = open("ToDo.txt", "r")
+lstTable = [] # defines the lstTable variable as a blank list because user will be 'appending' dictionary items to it.
+for line in objFile:
+    items = line.strip().split(",") # Reads entire first line of text file while splitting the line into two items.
 # 2) Load each row of data from the ToDo.txt file into a Python dictionary. The data will be stored in a row,
 # like a table.
-objFile = open("ToDo.txt", "r")
-lstTable = [] # assigns a blank list to variable because user will be 'appending' dictionary itmes to the list.
-
+    dicNewRow = {items[0]: items[1]} # Assigns each index item of line to a dictionary row.
 # 3) After you get the data into a Python dictionary, add the new dictionary 'row' into a Python 'list' object (now
-# the data will be managed as a table.)
-# Reads entire first line of text file while separating into two index items, 'task' and 'priority'.
-# Assigns each index item of line to a single variable split into its indices.
-# assigns 'items' to a new variable and makes that variable a dictionary item consisting of each index as the key/value.
-# Puts dictionary item into lstTable by 'append' method.
-for line in objFile:
-    items = line.strip().split(",")
-    dicNewRow = {items[0]: items[1]}
-    lstTable.append(dicNewRow)
+    # the data will be managed as a table.)
+    lstTable.append(dicNewRow) # Puts dictionary item into lstTable using 'append' method.
 # 4) Display contents of the list to user.
 print("Your current database is: ", lstTable)
 objFile.close()
@@ -31,28 +26,28 @@ while (strChoice != 0):
     """)
     strChoice = str(input("Which option would you like to perform? [0 - 4] - "))
     print()
-    # Exits the program.
-    if (strChoice == '0'):
-        print("Goodbye!")
-        break
+# 6) Allows user to save upon exiting the program.
+    if (strChoice.strip() == '0'):
+        objFile = open("ToDo.txt", "w")
+        objFile.write(str(lstTable))
+        objFile.close()
+    print("Your data was saved. Goodbye!")
+    break
 
     # Shows current data table
-    elif (strChoice.strip() == '1'):
+    if (strChoice.strip() == '1'):
         print("\nYour current data is:\n")
-        for index, item in enumerate(lstTable): # iterates across each item in lstTable by index number.
-            print("{} - {}".format(index, item)) # 'format' method allows user to rearange and display lstTable with each index
-            # number corresponding to its respective dictionary item. This will allow easy addition and deletion of items in
-            # lstTable according to their index numbers.
+        for index, item in enumerate(lstTable):
+            print("{} - {}".format(index, item))
 
-        print("Please select a user option:")
-        continue
+            print("Please select a user option:")
+            continue
 
-    elif (strChoice.strip() == '2'):
+    if (strChoice.strip() == '2'):
         # Ask user for two new user inputs (task, priority).
         strTask = input("Please specify the new task: ")
         strPriority = input("Please specify the priority: ")
-        dicNewItem = {strTask:strPriority} # assigns user inputs into a new dictionary item. This MUST be displayed in proper
-           # dictionary form (i.e. curly brackets and 'colon') otherwise it will be interpreted as a 'set' and will return an error.
+        dicNewItem = {strTask:strPriority}
 
         # writes that data to object file
         objFile = open("ToDo.txt", "a")
@@ -66,20 +61,21 @@ while (strChoice != 0):
         for line in objFile:
             strData = line
             dicNewItem = {(strData.split(",")[0]).strip(), (strData.split(",")[1]).strip()}
-            lstTable.append(dicNewItem) #adds new dictionary item to list using 'append' method.
+            lstTable.append(dicNewItem)
             # 4) Display contents of the list to user.
         print("Your new database is: ", lstTable)
         objFile.close()
 
-    elif (strChoice.strip() == '3'):
+    if (strChoice.strip() == '3'):
         strItemDelete = int(input("Please enter a task to delete: "))
         for index, item in enumerate(lstTable):
             if index == strItemDelete:
                 value = item
         lstTable.remove(value)
 
-    elif (strChoice.strip() == '4'):
+    # 6) Saves data in the ToDO.txt file
+    if (strChoice.strip() == '4'):
         objFile = open("ToDo.txt", "w")
         objFile.write(str(lstTable))
         objFile.close()
-    print("Thank you! Your data was saved.")
+        print("Thank you! Your data was saved.")
