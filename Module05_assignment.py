6e39e94e44a2150f640b73fe68ac610da269575e
runyanorama <runyanorama@users.noreply.github.com>
2017-05-07 17:14:38 -0500

    Update Module05_assignment.py

--- Module05_assignment.py
+++ Module05_assignment.py
@@ -2,12 +2,13 @@
 # 2) Load each row of data from the ToDo.txt file into a Python dictionary. The data will be stored in a row,
 # like a table.
 objFile = open("ToDo.txt", "r")
-lstTable = []
+lstTable = [] # assigns a blank list to variable because user will be 'appending' dictionary itmes to the list.
 
 # 3) After you get the data into a Python dictionary, add the new dictionary 'row' into a Python 'list' object (now
 # the data will be managed as a table.)
 # Reads entire first line of text file while separating into two index items, 'task' and 'priority'.
-# Assigns each index item of line to a dictionary item.
+# Assigns each index item of line to a single variable split into its indices.
+# assigns 'items' to a new variable and makes that variable a dictionary item consisting of each index as the key/value.
 # Puts dictionary item into lstTable by 'append' method.
 for line in objFile:
     items = line.strip().split(",")
@@ -38,8 +39,10 @@ while (strChoice != 0):
     # Shows current data table
     elif (strChoice.strip() == '1'):
         print("\nYour current data is:\n")
-        for index, item in enumerate(lstTable):
-            print("{} - {}".format(index, item))
+        for index, item in enumerate(lstTable): # iterates across each item in lstTable by index number.
+            print("{} - {}".format(index, item)) # 'format' method allows user to rearange and display lstTable with each index
+            # number corresponding to its respective dictionary item. This will allow easy addition and deletion of items in
+            # lstTable according to their index numbers.
 
         print("Please select a user option:")
         continue
@@ -48,7 +51,8 @@ while (strChoice != 0):
         # Ask user for two new user inputs (task, priority).
         strTask = input("Please specify the new task: ")
         strPriority = input("Please specify the priority: ")
-        dicNewItem = {strTask:strPriority}
+        dicNewItem = {strTask:strPriority} # assigns user inputs into a new dictionary item. This MUST be displayed in proper
+           # dictionary form (i.e. curly brackets and 'colon') otherwise it will be interpreted as a 'set' and will return an error.
 
         # writes that data to object file
         objFile = open("ToDo.txt", "a")
@@ -62,7 +66,7 @@ while (strChoice != 0):
         for line in objFile:
             strData = line
             dicNewItem = {(strData.split(",")[0]).strip(), (strData.split(",")[1]).strip()}
-            lstTable.append(dicNewItem)
+            lstTable.append(dicNewItem) #adds new dictionary item to list using 'append' method.
             # 4) Display contents of the list to user.
         print("Your new database is: ", lstTable)
         objFile.close()
